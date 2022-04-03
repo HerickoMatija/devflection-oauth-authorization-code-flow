5194fa5b39db9173c7658564cc11bbcc25795905
Matija <matija.hericko@gmail.com>
2022-04-03 15:46:58 +0200

    Added redirection on button click

--- frontend/src/components/auth/Auth.js
+++ frontend/src/components/auth/Auth.js
@@ -1,9 +1,16 @@
 import './Auth.css';
 
+const START_OAUTH = "http://localhost:5000/startOAuthFlow"
+
 function Auth() {
+
+    function redirect() {
+        window.location.href = START_OAUTH
+    }
+
     return (
         <div className='auth-container'>
-            <button>
+            <button onClick={redirect}>
                 Get the access token!
             </button>
         </div>
