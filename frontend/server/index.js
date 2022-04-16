e72f467ab2c62c103dcedbf55474592ea16ba79e
Matija <matija.hericko@gmail.com>
2022-04-16 22:09:42 +0200

    implementing code exchange, work in progress

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -1,5 +1,5 @@
 const { EXPRESS_PLAYERS } = require('./expressData');
-const { AUTHORIZE_REQUEST_URL } = require('./oauth.js');
+const { AUTHORIZE_REQUEST_URL, TOKEN_ENDPOINT_URL, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = require('./oauth.js');
 const express = require("express");
 const path = require("path");
 var cors = require('cors')
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, "..", "build")));
 app.get("/myFavouritePlayersInTheNBA", (req, res) => {
     if (isValid()) {
         // TODO get data, merge it with local and return
-    } else if(refreshToken) {
+    } else if (refreshToken) {
         // TODO refresh and then get data, merge with local etc..        
     } else {
         res.send(EXPRESS_PLAYERS);
@@ -27,10 +27,26 @@ app.get("/startOAuthFlow", (req, res) => {
     res.redirect(AUTHORIZE_REQUEST_URL);
 });
 
-app.get("/authorizationCallback", (req, res) => {
-    console.log(req.query.code);
-    // TODO exchange the code for the acces token
-    res.send("Callback was called");
+app.get("/authorizationCallback", async (req, res) => {
+    await fetch(TOKEN_ENDPOINT_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+        body: new URLSearchParams({
+            'grant_type': 'authorization_code',
+            'code': req.query.code,
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET,
+            'redirect_uri': REDIRECT_URI
+        })
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw response;
+    }).then(data => res.send(data))
+    .catch(err => res.send(err));
 });
 
 app.get("/getAccessToken", (req, res) => {
@@ -42,7 +58,7 @@ app.get("/getRefreshToken", (req, res) => {
 });
 
 app.listen(5000, () => {
-  console.log("Express server started on port 5000");
+    console.log("Express server started on port 5000");
 });
 
 
