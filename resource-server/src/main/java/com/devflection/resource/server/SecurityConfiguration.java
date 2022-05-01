ed4d661b9b0e820e067261d89e2ebed2b84627af
Matija <matija@devflection.com>
2022-05-01 20:00:06 +0200

    Formatting

--- resource-server/src/main/java/com/devflection/resource/server/SecurityConfiguration.java
+++ resource-server/src/main/java/com/devflection/resource/server/SecurityConfiguration.java
@@ -4,16 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {    
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests(authz -> authz
-                .antMatchers(HttpMethod.GET, "/favouritePlayers").hasAuthority("SCOPE_devflectionPlayers")
-                .anyRequest().authenticated())
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+        http.authorizeRequests(
+                authorizationRequest -> authorizationRequest.antMatchers(HttpMethod.GET, "/favouritePlayers")
+                        .hasAuthority("SCOPE_devflectionPlayers")
+                        .anyRequest()
+                        .authenticated()
+        ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
 }
